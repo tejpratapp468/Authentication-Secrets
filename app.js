5b556c5585b5b3d8371b0504efbba7059b99afc3
Pandey Tejpratap <tejpratapp468@gmail.com>
2021-01-23 22:04:22 +0530

    Level 5 security using Passport,passport-local,passport-local-mongoose,express-session

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ const express=require("express");
 const bodyParser=require("body-parser");
 const ejs=require("ejs");
 const mongoose=require("mongoose");
-//const encrypt=require("mongoose-encryption");
-//const md5=require("md5");
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+const session = require('express-session');
+const passport=require("passport");
+const passportLocalMongoose=require("passport-local-mongoose");
 
 const app=express();
 
@@ -15,9 +14,25 @@ app.use(express.static("public"));
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 
+//Its really important to place your code in the same order
+//we have initialized session telling our app to use session package
+app.use(session({
+  //setting up initial configuration
+  secret:"Our little secret.",
+  resave:false,
+  saveUninitialized:false
+}));
+
+// to initialize passport
+app.use(passport.initialize());
+
+//telling our app to use passport to set up our session
+app.use(passport.session());
+
 //to connect to our mongoDB
 mongoose.connect("mongodb://localhost:27017/userDB",
 {useNewUrlParser: true,useUnifiedTopology: true,useFindAndModify: false });
+mongoose.set("useCreateIndex", true);
 
 //this schema is no longer simple a JS object but it is an object created from mongoose schema class.
 const userSchema=new mongoose.Schema({
@@ -25,12 +40,16 @@ const userSchema=new mongoose.Schema({
   password:String
 });
 
-//level 2//userSchema.plugin(encrypt, {secret:process.env.SECRET, encryptedFields: ["password"]});
-/*In plugin we have added encryption package to userSchema by encryption
-we have added secret that we will use to encrypt our Password through secret:secrets
-we have added fields that we want to encrypt through encryptedFields*/
+//set up for passportLocalMongoose to plugin userSchema with passportLocalMongoose it has to be a mongoose schema
+userSchema.plugin(passportLocalMongoose);
 
 const User=mongoose.model("User",userSchema);
+
+//default code for using passportLocalMongoose from its documentation
+passport.use(User.createStrategy()); //creating local login strategy
+
+passport.serializeUser(User.serializeUser());//to serialize user
+passport.deserializeUser(User.deserializeUser());//to deserialize user
 
 app.get("/",function(req,res){
   res.render("home");
@@ -44,48 +63,64 @@ app.get("/register",function(req,res){
   res.render("register");
 });
 
-app.post("/register",function(req,res){
-  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-      // Store hash in your password DB.
-      const newUser=new User({
-        email:req.body.username,
-        password:hash
-      });
-      newUser.save(function(err){
-        if(err){
-          console.log(err);
-        } else{
-          res.render("secrets");
-        }
-      });
-  });
+app.get("/secrets",function(req,res){
+  //if the user is authenticated means user is already logged in here we rely on session,passport,passport local,passportLocalMongoose
+//only then render secrets page
+if(req.isAuthenticated()){
+  res.render("secrets");
+} else{
+  res.redirect("/login");
+}
 
+});
+
+app.get("/logout",function(req,res){
+  //Here we deauthenticate user and end that user session
+  req.logout();
+  res.redirect("/");
+});
+
+app.post("/register",function(req,res){
+//this register method come from passport local mongoose
+User.register({username:req.body.username},req.body.password,function(err,user){
+  if(err){
+    console.log(err);
+    res.redirect("/register");
+  } else{
+    //if no err then we will try to authenticate user by local strategy
+    passport.authenticate("local")(req,res,function(){
+      /*this callback function only triggered if authentication was successful and
+      we manage setup cookie that saves user current login session*/
+
+      res.redirect("/secrets"); //Now no need to render secrets page user can directly go to secrets page
+                                //by /secrets.Thanks to cookies!!
+    })
+  }
+})
 
 });
 
 app.post("/login",function(req,res){
-  const username=req.body.username;
-  const password=req.body.password;//we will hash the user's password using same hash function md5
 
-  User.findOne({email:username},function(err,foundUser){ /*when this findOne function is called then mongoose
-    will decrypt password field to compare it with password entered by user*/
-    if(err){
-      console.log(err);
-    } else{
-      if(foundUser){
-        bcrypt.compare(password,foundUser.password , function(err, result) {
-           // result == true
-           if(result===true){
-             res.render("secrets");
-           }
-           else{
-             res.send("Invalid login credentials");
-           }
-        });
+const user=new User({
+  username:req.body.username,
+  password:req.body.password
+});
 
-      }
-    }
-  });
+//this login method come from passport
+req.login(user,function(err){  //user came from login credentials
+  if(err){
+    console.log(err);
+  } else{
+    passport.authenticate("local") (req,res,function(){
+      /*this callback function only triggered if authentication was successful and
+      we manage setup cookie that saves user current login session*/
+
+      res.redirect("/secrets"); //Now no need to render secrets page user can directly go to secrets page
+                                //by /secrets.Thanks to cookies!!
+    });
+  }
+});
 
 });
 
